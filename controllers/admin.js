7ecadfcaf479cbcffd3da2576e1acbe94e01ebbd
ruchitakb7 <ruchitakb7@gmail.com>
2024-04-23 04:10:36 +0530

    task 10

--- controllers/admin.js
+++ controllers/admin.js
@@ -78,8 +78,8 @@ exports.postEditProduct = (req, res, next) => {
 
 exports.getProducts = (req, res, next) => {
   Product.find()
-    .select(' title price -_id')
-    .populate('userId','name')
+   // .select(' title price -_id')
+   // .populate('userId','name')
     .then(products => {
       console.log(products)
       res.render('admin/products', {
--- controllers/shop.js
+++ controllers/shop.js
@@ -44,54 +44,31 @@ exports.getIndex = (req, res, next) => {
 
 exports.getCart = (req, res, next) => {
   req.user
-    .getCart()
-    .then(cart => {
-      return cart
-        .getProducts()
-        .then(products => {
+    .populate('cart.items.productId')
+   // execPopulate()
+    .then(user=> {
+     console.log(user.cart.items);
+      const products=user.cart.items
           res.render('shop/cart', {
             path: '/cart',
             pageTitle: 'Your Cart',
             products: products
           });
         })
-        .catch(err => console.log(err));
-    })
-    .catch(err => console.log(err));
+        .catch(err => console.log(err))
+   
 };
 
 exports.postCart = (req, res, next) => {
   const prodId = req.body.productId;
-  let fetchedCart;
-  let newQuantity = 1;
-  req.user
-    .getCart()
-    .then(cart => {
-      fetchedCart = cart;
-      return cart.getProducts({ where: { id: prodId } });
-    })
-    .then(products => {
-      let product;
-      if (products.length > 0) {
-        product = products[0];
-      }
+  Product.findById(prodId)
+  .then(product =>{
+    return req.user.addToCart(product)
+  })
+  .then(result=>{
+    res.redirect('/cart');
+  })
 
-      if (product) {
-        const oldQuantity = product.cartItem.quantity;
-        newQuantity = oldQuantity + 1;
-        return product;
-      }
-      return Product.findById(prodId);
-    })
-    .then(product => {
-      return fetchedCart.addProduct(product, {
-        through: { quantity: newQuantity }
-      });
-    })
-    .then(() => {
-      res.redirect('/cart');
-    })
-    .catch(err => console.log(err));
 };
 
 exports.postCartDeleteProduct = (req, res, next) => {
